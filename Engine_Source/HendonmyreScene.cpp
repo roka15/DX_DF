bdd1a165047a6ccf82a74e393992a930311eadd7
roka15 <rokaisback0105@gmail.com>
2023-06-29 15:41:47 +0900

    배경화면 띄우기2 배경화면 npk 파일  업로드

---
+++ Engine_Source/HendonmyreScene.cpp
@@ -0,0 +1,81 @@
+#include "HendonmyreScene.h"
+#include "Resources.h"
+#include "MeshRenderer.h"
+#include "Transform.h"
+#include "Camera.h"
+#include "CameraScript.h"
+
+#include "Mesh.h"
+#include "Material.h"
+#include "NPK.h"
+namespace roka
+{
+	HendonmyreScene::HendonmyreScene():Scene(ESceneType::AnotherVillage)
+	{
+	}
+
+	HendonmyreScene::~HendonmyreScene()
+	{
+	}
+
+	void HendonmyreScene::Initialize()
+	{
+		Scene::Initialize();
+	}
+
+	void HendonmyreScene::Update()
+	{
+		Scene::Update();
+	}
+
+	void HendonmyreScene::LateUpdate()
+	{
+		Scene::LateUpdate();
+	}
+
+	void HendonmyreScene::Render()
+	{
+		Scene::Render();
+	}
+
+	void HendonmyreScene::OnExit()
+	{
+		Scene::OnExit();
+	}
+
+	void HendonmyreScene::OnEnter()
+	{
+		Scene::OnEnter();
+		GameObject* bg = new GameObject();
+		AddGameObject(ELayerType::BackObject, bg);
+		MeshRenderer* mr = bg->AddComponent<MeshRenderer>();
+		mr->mesh = Resources::Find<Mesh>(L"RectMesh");
+		mr->material = Resources::Find<Material>(L"BGMaterial");
+
+		Transform* tf = bg->GetComponent<Transform>();
+		tf->position = Vector3(0.0f, 0.0f, 0.0f);
+		tf->scale = Vector3(40.0f, 9.0f, 1.0f);
+		{
+			std::shared_ptr<NPK> npk = Resources::Find<NPK>(L"mapnpk");
+			if (npk == nullptr)
+				npk = Resources::Load<NPK>(L"mapnpk", L"..\\Resources\\npk\\map.npk");
+
+			std::shared_ptr<Texture> texture = npk->GetTexture(L"hendonmyre", 0);
+			Resources::Insert(L"hendonmyreTexture", texture);
+			mr->material->texture = texture;
+		}
+
+		{
+			GameObject* camera = new GameObject();
+			AddGameObject(ELayerType::Player, camera);
+			camera->AddComponent<CameraScript>();
+			Camera* cameraComp = camera->AddComponent<Camera>();
+			camera->GetComponent<Transform>()->position = Vector3(0.0f, 0.0f, -10.0f);
+		}
+	}
+
+	void HendonmyreScene::Loading()
+	{
+	}
+
+}
---
+++ Engine_Source/HendonmyreScene.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Scene.h"
+
+namespace roka
+{
+	class HendonmyreScene :public Scene
+	{
+	public:
+		HendonmyreScene();
+		~HendonmyreScene();
+
+		virtual void Initialize();
+		virtual void Update();
+		virtual void LateUpdate();
+		virtual void Render();
+
+		virtual void OnExit();
+		virtual void OnEnter();
+		virtual void Loading()override;
+	private:
+	};
+}
+
+
