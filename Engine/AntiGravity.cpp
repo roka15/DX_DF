1336bf60d4bf131679985b6c4e9d90775b581fab
roka15 <rokaisback0105@gmail.com>
2023-09-20 21:53:43 +0900

    anti gravity pluto 출력 내일 눈 높이 조절 필요한 프레임에 해줘야함

--- Engine/AntiGravity.cpp
+++ Engine/AntiGravity.cpp
@@ -6,6 +6,7 @@
 #include "SceneManager.h"
 #include "NPK.h"
 #include "Resources.h"
+#include "RokaTime.h"
 
 #include "Animator.h"
 #include "Transform.h"
@@ -42,17 +43,18 @@ namespace roka
 			if (effectObj != nullptr)
 				return;
 			effectObj = pool->GetPool(L"AniObject")->Spawn();
-			effectObj->SetName(key+L"Obj");
+			effectObj->SetName(key + L"Obj");
 			std::shared_ptr<Texture> texture1
 				= Resources::Find<NPK>(L"homonculouse")->CreateAtlas(L"fluto.img", 39, 53, L"mage_homon_fluto");
 			std::shared_ptr<Animator> ani = effectObj->GetComponent<Animator>();
 			ani->Create(texture1, L"mage_antiGravity_homon1", 8, 14, 0.05f);
-			ani->Create(texture1, L"mage_antiGravity_homon2", 11, 14, 0.05f);
+			ani->Create(texture1, L"mage_antiGravity_homon2", 12, 14, 0.05f);
 			aniPtr = ani.get();
 			ani->CompleteEvent(L"mage_antiGravity_homon1") = std::bind([aniPtr]()->void {aniPtr->PlayAnimation(L"mage_antiGravity_homon2"); });
 			ani->PlayAnimation(L"mage_antiGravity_homon1");
+			ani->CompleteEvent(L"mage_antiGravity_homon1") = std::bind(&AntiGravity::DeSpawnEffect, this, caster, key+L"_pluto");
 			ani->CompleteEvent(L"mage_antiGravity_homon2") = std::bind(&AntiGravity::DeSpawnEffect, this, caster, key);
-			SceneManager::GetActiveScene()->AddGameObject(ELayerType::BackObject,effectObj);
+			SceneManager::GetActiveScene()->AddGameObject(ELayerType::BackObject, effectObj);
 
 			scale.x = 1.15f;
 			scale.y = 1.15f;
@@ -69,11 +71,31 @@ namespace roka
 			std::shared_ptr<Texture> texture1
 				= Resources::Find<NPK>(L"mageAntiGravity")->CreateAtlas(L"pluto-normal.img", 0, 16, L"mage_antiGravity_pluto");
 			std::shared_ptr<Animator> ani = effectObj->GetComponent<Animator>();
-			ani->Create(texture1, L"mage_antiGravity_pluto", 0, 15, 0.05f);
-			ani->PlayAnimation(L"mage_antiGravity_pluto");
+			ani->Create(texture1, L"mage_antiGravity_pluto_start", 0, 12, 0.01f);
+			ani->Create(texture1, L"mage_antiGravity_pluto_middle", 12, 16, 0.06f);
+			ani->PlayAnimation(L"mage_antiGravity_pluto_start");
 			aniPtr = ani.get();
-			ani->CompleteEvent(L"mage_antiGravity_pluto") = std::bind([aniPtr]()->void {aniPtr->PlayReverseAnimation(L"mage_antiGravity_pluto"); });
-			ani->ReverseCopleteEvent(L"mage_antiGravity_pluto") = std::bind(&AntiGravity::DeSpawnEffect,this,caster,key);
+			ani->SetFrameEventListener(this);
+			std::shared_ptr<Animation> animation = ani->FindAnimation(L"mage_antiGravity_pluto_start");
+			animation->RegisterFrameEvent(L"mage_antiGravity_pluto_start", 11);
+			animation->RegisterFrameEvent(L"mage_antiGravity_pluto_eye", 5);
+			ani->CompleteEvent(L"mage_antiGravity_pluto_middle") = std::bind(&AntiGravity::DeSpawnEffect, this, caster, key);
+			ani->ReverseCopleteEvent(L"mage_antiGravity_pluto_start") = std::bind(&AntiGravity::DeSpawnEffect, this, caster, key+L"_reverse");
+
+			std::shared_ptr<Texture> eye_texture
+				= Resources::Find<NPK>(L"mageAntiGravity")->CreateAtlas(L"pluto-eye.img", 0, 1, L"mage_antiGravity_pluto_eye");
+			std::shared_ptr<GameObject> eye = pool->GetPool(L"AniEftObject")->Spawn();
+			eye->SetName(L"plutoEyeObj");
+			std::shared_ptr<Animator> eyeAni = eye->GetComponent<Animator>();
+			eyeAni->Create(eye_texture, L"mage_anitGravity_pluto_eye", 0, 0, 0.0f);
+			{
+				std::shared_ptr<Transform> tf = eye->GetComponent<Transform>();
+				tf->scale = Vector3(0.4f,0.3f,1.0f);
+				tf->position = tf->position + Vector3(-0.1f, 0.0f, 0.0f);
+			}
+
+			effectObj->AddChild(eye);
+
 			SceneManager::GetActiveScene()->AddGameObject(ELayerType::BackObject, effectObj);
 
 			scale.x = 0.5f;
@@ -101,17 +123,30 @@ namespace roka
 		std::shared_ptr<GameObject> deleteObj;
 		name = key + L"Obj";
 		deleteObj = SceneManager::GetActiveScene()->FindGameObject(ELayerType::BackObject, name);
+		std::shared_ptr<Animator> ani;
+		bool mbActive = false;
 		if (deleteObj != nullptr)
 		{
-			deleteObj->GetComponent<Animator>()->ActiveAnimationNull();
+			ani = deleteObj->GetComponent<Animator>();
 		}
-		if (key.compare(L"homonculouse") == 0)
+		if (key.compare(L"homonculouse_pluto") == 0)
 		{
 			SpawnEffect(caster, L"pluto");
+			mbActive = true;
 		}
-
-		if (deleteObj != nullptr)
+		else if (key.compare(L"pluto") == 0)
 		{
+				ani->PlayReverseAnimation(L"mage_antiGravity_pluto_start");
+				mbActive = true;
+		}
+		else if (key.compare(L"pluto_reverse") == 0)
+		{
+			deleteObj = SceneManager::GetActiveScene()->FindGameObject(ELayerType::BackObject, L"plutoObj");
+			ani = deleteObj->GetComponent<Animator>();
+		}
+		if (mbActive == false && deleteObj !=nullptr)
+		{
+			ani->ActiveAnimationNull();
 			deleteObj->active = GameObject::EState::Dead;
 		}
 	}
@@ -141,9 +176,25 @@ namespace roka
 	}
 	void AntiGravity::OnAnimationFramEvent(std::shared_ptr<GameObject> caster, std::wstring frameEvent)
 	{
-		if (frameEvent.compare(L"SpawnHomonculouse")==0)
+		if (frameEvent.compare(L"SpawnHomonculouse") == 0)
 		{
 			SpawnEffect(caster, L"homonculouse");
+		}
+		else if (frameEvent.compare(L"mage_antiGravity_pluto_start") == 0)
+		{
+			std::shared_ptr<Animator> ani = caster->GetComponent<Animator>();
+			Animator* aniPtr = ani.get();
+			Time::CallBackTimerInfo callBack = {};
+			callBack.endTime = 0.05f;
+			Time::RequestEvent(callBack, std::bind([aniPtr]()->void {
+				aniPtr->PlayAnimation(L"mage_antiGravity_pluto_middle"); 
+				//aniPtr->owner->GetChild(L"plutoEyeObj")->GetComponent<Tra
+				}));
+		}
+		else if (frameEvent.compare(L"mage_antiGravity_pluto_eye") == 0)
+		{
+			std::shared_ptr<GameObject> obj = caster->GetChild(L"plutoEyeObj");
+			obj->GetComponent<Animator>()->PlayAnimation(L"mage_anitGravity_pluto_eye",true);
 		}
 	}
 	void AntiGravity::OnCollisionEnter(std::shared_ptr<GameObject> caster, std::shared_ptr<GameObject> target)
