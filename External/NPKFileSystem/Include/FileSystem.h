24568d8600192c8634487b35cd6da763c1d146d4
roka15 <rokaisback0105@gmail.com>
2023-09-20 18:33:50 +0900

    프로젝트 정보 업데이트 NPK system 추가 함수 = SaveCsv 및 문자열 해제 관련해서 수정점이 있어서 업데이트함.

--- External/NPKFileSystem/Include/FileSystem.h
+++ External/NPKFileSystem/Include/FileSystem.h
@@ -32,7 +32,7 @@ namespace roka::file
 			memcpy(buffer, ref.buffer, length);
 		}
 		FileInfo(std::string _name, size_t _len, char* _buffer) :name(_name), length(_len), buffer(_buffer) {};
-		~FileInfo() { delete buffer; }
+		~FileInfo() { delete [] buffer; }
 		std::string name;
 		std::string parent_path;
 		size_t length;
@@ -105,6 +105,7 @@ namespace roka::file
 		NPKSystem() :mCsvLine(0), mCSVBuffers(nullptr) {};
 		virtual ~NPKSystem() { Release(); }
 		void SavePacks(std::string _save_path, std::map<std::string, PackInfo*> _pack);
+		void SaveCsvs(std::string _save_path, std::map<std::string, CSVInfo*> _csv);
 		const FileInfo* CreateImagePackage(std::string _read_path, std::string _road_format);
 		size_t ReadImagePackage(const char* _buf, std::map<std::string, CSVInfo*>& _csvmap, std::map<std::string, PackInfo*>& _packmap);
 		void CreateNPK(std::string _image_path, std::string _txt_path, std::string _format, std::string _save_path);
